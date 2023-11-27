f7eb600a8818e8b6c8a92dc27d07d8d3695aeb54
BrianChen027 <brianking80227@gmail.com>
2023-11-27 01:08:50 -0500

    Update Home page

---
+++ src/app/components/layout/SectionHeaders.js
@@ -0,0 +1,8 @@
+export default function SectionHeaders({subHeader,mainHeader}){
+    return (
+        <>
+            <h3 className="uppercase text-gray-500 font-semibold leading-4">Check out</h3>
+            <h2 className="text-coffee font-bold text-4xl italic">Menu</h2>
+        </>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Header from './components/layout/Header'
 import Hero from './components/layout/Hero'
 import HomeMenu from './components/layout/HomeMenu'
+import SectionHeaders from './components/layout/SectionHeaders'
 
 export default function Home() {
   return (
@@ -10,6 +11,36 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className='text-center my-16'>
+        <SectionHeaders
+          subHeader={'Our Story'}
+          mainHeader={'About us'} />
+        <div className='max-w-xl mx-auto mt-4 text-gray-500 flex flex-col gap-4'>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
+          </p>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          </p>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          </p>
+        </div>
+
+      </section>
+
+      <section className='text-center my-8'>
+        <SectionHeaders 
+          subHeader={'Don\'t hesitate'}
+          mainHeader={'Contact us'} />
+        <a className="text-4xl" herf=""></a>
+      </section>
     </>
   )
 }
